c001af26d2ac68532c78e514ee52f3906d246ba1
Leandro Piovezan da Silva <leandro.piovezan@yahoo.com.br>
2024-01-15 07:38:04 -0300

    .

--- TestesUnitarios.Desafio.Console/Program.cs
+++ TestesUnitarios.Desafio.Console/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+using TestesUnitarios.Desafio.Console.Services;
+
+ValidacoesLista vLista = new ValidacoesLista();
+
+var lista = new List<int> { 2, 4, 6, -2, -5 };
+var listaSemNegativos = vLista.RemoverNumerosNegativos(lista);
+
+var contemNumero = vLista.ListaContemDeterminadoNumero(lista,6);
+
+var multiplica = vLista.MultiplicarNumerosLista(lista, 2);
+
+var retornaMaior = vLista.RetornarMaiorNumeroLista(lista);
+var retornaMenor = vLista.RetornarMenorNumeroLista(lista);
+
+Console.WriteLine($"Essa é a Lsita original {string.Join(", ", lista)}");
+Console.WriteLine($"Essa é a lista retirando os negativos {string.Join(",",listaSemNegativos)}");
+Console.WriteLine($"Contém número procurado? -{contemNumero}");
+Console.WriteLine($"Multiplicar número lista -{string.Join(",",multiplica)}");
+Console.WriteLine($"Retorna Maior -{string.Join(",",retornaMaior)}");
+Console.WriteLine($"Retorna Menor -{string.Join(",",retornaMenor)}");
